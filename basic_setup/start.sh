6c5985ffe992bd8d121672ced609c2066c86f0de
dantol29 <dantol29@gmail.com>
2025-01-29 23:07:33 +0200

    add context canister

--- basic_setup/start.sh
+++ basic_setup/start.sh
@@ -4,7 +4,7 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 RESET='\033[0m'
 
-echo -e "${GREEN}Compiling context, proxy and external canister...${RESET}"
+echo -e "${GREEN}Compiling app, proxy, external and context canister...${RESET}"
 
 bash context/build.sh
 bash proxy_contract/build.sh
@@ -54,6 +54,27 @@ if [ $? -ne 0 ] ; then
     fi
 fi
 
+# Create context canister
+dfx canister id context_contract > /dev/null
+if [ $? -ne 0 ] ; then
+  echo -e "${GREEN}Creating context canister...${RESET}"
+  dfx canister create context_contract
+fi
+
+dfx canister call context_contract proxy_contract > /dev/null 2>&1
+if [ $? -ne 0 ] ; then
+  echo -e "${GREEN}Uploading WASM for context canister...${RESET}"
+
+  EXTERNAL_ID=$(dfx canister id external_contract)
+  dfx canister install context_contract --mode=install --wasm ./context_contract/res/context_contract.wasm --argument "(principal \"${EXTERNAL_ID}\")"
+
+  # dfx canister call context_contract proxy_contract > /dev/null 2>&1
+  #   if [ $? -ne 0 ] ; then
+  #       echo -e "${RED}ERROR: could not reach context canister.${RESET}"
+  #       exit 1
+  #   fi
+fi
+
 echo -e "${GREEN}Installation done! Starting node${RESET}"
 
 # TODO: start the node and install the context
@@ -61,4 +82,8 @@ echo -e "${GREEN}Installation done! Starting node${RESET}"
 # merod --node-name node1 init --server-port 2430 --swarm-port 2530
 # merod --node-name node2 init --server-port 2431 --swarm-port 2531
 # merod --node-name node1 run
-# application install file ./context/res/context.wasm
+# application install file /Users/tolmadan/Desktop/kv-store/calimero_x_icp_hackathon/basic_setup/context/res/kv_store.wasm
+
+# Cf3SwcyF9gcC9j1YNfgkDvHkKzjnpajs4cz7QQtt5yD5
+
+# 975PyY76E8X1qMdExt7B9SimtCnDeXn6J3kyuf3MiWuK
