02cc917fa7596f6fb0e1bdf84600ebedc5e19160
dantol29 <dantol29@gmail.com>
2025-01-30 15:28:46 +0200

    add rm

--- basic_setup/start.sh
+++ basic_setup/start.sh
@@ -6,6 +6,9 @@ RESET='\033[0m'
 
 echo -e "${GREEN}Compiling app, proxy, external and context canister...${RESET}"
 
+USER=$(whoami)
+rm -rf /Users/${USER}/.calimero/node1
+
 bash context/build.sh
 bash proxy_contract/build.sh
 bash external_contract/build.sh
@@ -61,29 +64,31 @@ if [ $? -ne 0 ] ; then
   dfx canister create context_contract
 fi
 
-dfx canister call context_contract proxy_contract > /dev/null 2>&1
+dfx canister logs context_contract > /dev/null 2>&1
 if [ $? -ne 0 ] ; then
   echo -e "${GREEN}Uploading WASM for context canister...${RESET}"
 
   EXTERNAL_ID=$(dfx canister id external_contract)
   dfx canister install context_contract --mode=install --wasm ./context_contract/res/context_contract.wasm --argument "(principal \"${EXTERNAL_ID}\")"
 
-  # dfx canister call context_contract proxy_contract > /dev/null 2>&1
-  #   if [ $? -ne 0 ] ; then
-  #       echo -e "${RED}ERROR: could not reach context canister.${RESET}"
-  #       exit 1
-  #   fi
+  dfx canister call context_contract logs > /dev/null 2>&1
+    if [ $? -ne 0 ] ; then
+        echo -e "${RED}ERROR: could not reach context canister.${RESET}"
+        exit 1
+    fi
 fi
 
 echo -e "${GREEN}Installation done! Starting node${RESET}"
 
-# TODO: start the node and install the context
+merod --node-name node1 init --server-port 2430 --swarm-port 2530
+merod --node-name node1 run
 
-# merod --node-name node1 init --server-port 2430 --swarm-port 2530
 # merod --node-name node2 init --server-port 2431 --swarm-port 2531
-# merod --node-name node1 run
-# application install file /Users/tolmadan/Desktop/kv-store/calimero_x_icp_hackathon/basic_setup/context/res/kv_store.wasm
 
-# Cf3SwcyF9gcC9j1YNfgkDvHkKzjnpajs4cz7QQtt5yD5
+# Cf3SwcyF9gcC9j1YNfgkDvHkKzjnpajs4cz7QQtt5yD5 - kv_store
 
-# 975PyY76E8X1qMdExt7B9SimtCnDeXn6J3kyuf3MiWuK
+# 975PyY76E8X1qMdExt7B9SimtCnDeXn6J3kyuf3MiWuK - context_contract
+
+application install file /Users/${USER}/Desktop/kv-store/calimero_x_icp_hackathon/basic_setup/context/res/kv_store.wasm
+
+context create --protocol icp 975PyY76E8X1qMdExt7B9SimtCnDeXn6J3kyuf3MiWuK
