2a62965719f50b5afa53159e74db8c84ce228167
552020 <stefanolombardo@posteo.de>
2025-02-02 19:16:13 +0100

    Feat: add AccessTokenWrapper from SDK and create getNodeUrl into node.ts utils

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,10 +1,14 @@
+// For AccessTokenWrapper check https://calimero-network.github.io/developer-tools/SDK/client-sdk/client-ts-sdk/#2-token-management
+import { AccessTokenWrapper } from "@calimero-network/calimero-client";
+import { getNodeUrl } from "./utils/node";
+
 function App() {
   return (
-    <>
+    <AccessTokenWrapper getNodeUrl={getNodeUrl}>
       <div>
         <h1>Hello BILLO!</h1>
       </div>
-    </>
+    </AccessTokenWrapper>
   );
 }
 
