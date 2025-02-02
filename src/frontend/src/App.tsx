f4f98437addb91764a2fbc9f654c86110fe22bfc
552020 <stefanolombardo@posteo.de>
2025-02-02 19:57:31 +0100

    chore: scaffold structure of App.tsx and add main pages/routes

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,13 +1,22 @@
 // For AccessTokenWrapper check https://calimero-network.github.io/developer-tools/SDK/client-sdk/client-ts-sdk/#2-token-management
 import { AccessTokenWrapper } from "@calimero-network/calimero-client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SetupPage from "./routes/SetupPage";
+import AuthenticationPage from "./routes/AuthenticationPage";
+import HomePage from "./routes/HomePage";
+
 import { getNodeUrl } from "./utils/node";
 
 function App() {
   return (
     <AccessTokenWrapper getNodeUrl={getNodeUrl}>
-      <div>
-        <h1>Hello BILLO!</h1>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SetupPage />} />
+          <Route path="/auth" element={<AuthenticationPage />} />
+          <Route path="/home" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </AccessTokenWrapper>
   );
 }
