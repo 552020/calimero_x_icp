443d597cce6147f85bbd35a193cde02ae92f018f
552020 <stefanolombardo@posteo.de>
2025-02-03 00:07:11 +0100

    Style: tweak ContentWrapper

--- src/frontend/src/components/ContentWrapper.tsx
+++ src/frontend/src/components/ContentWrapper.tsx
@@ -1,32 +1,28 @@
 import { Footer } from "@/components/Footer";
 import CalimeroLogo from "@/assets/calimero-logo.svg";
-
+import ICPLogo from "@/assets/icp-logo.svg";
 interface ContentWrapperProps {
   children: React.ReactNode;
 }
 
 export const ContentWrapper = ({ children }: ContentWrapperProps) => {
   return (
-    <div className="h-screen w-full bg-gray-900">
+    <div className="min-h-screen w-full bg-pink-500">
       {/* Navigation Bar */}
-      <nav className="flex justify-between px-24 py-4">
+      <nav className="flex justify-between px-2 py-4 bg-green-500">
         <div className="relative flex items-center gap-2">
           <img
             src={CalimeroLogo}
             alt="Calimero Admin Dashboard Logo"
             className="h-[43.3px] w-[160px]"
           />
-          <h4 className="absolute left-12 top-8 text-xs text-white">
-            Calimero Network
-          </h4>
+          <img src={ICPLogo} alt="ICP Logo" className="h-[43.3px] w-[160px]" />
         </div>
       </nav>
 
       {/* Main Content */}
-      <div className="flex h-[calc(100vh-75.3px)] items-center justify-center text-white">
-        <div className="flex flex-col justify-center">{children}</div>
-        <Footer />
-      </div>
+      <div className="flex flex-col justify-center">{children}</div>
+      <Footer />
     </div>
   );
 };
